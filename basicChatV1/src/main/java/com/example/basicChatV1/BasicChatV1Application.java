2277c351b2799dd177cad412955fd2a700c8937d
gamer_21 <sajal.ua@gmail.com>
2025-09-10 12:18:02 +0530

    Add files via upload

---
+++ basicChatV1/src/main/java/com/example/basicChatV1/BasicChatV1Application.java
@@ -0,0 +1,13 @@
+package com.example.basicChatV1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicChatV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicChatV1Application.class, args);
+	}
+
+}
