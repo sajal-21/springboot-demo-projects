8d04b70608b7363a8a7fbc8b36f0473918614569
gamer_21 <sajal.ua@gmail.com>
2025-09-10 12:11:19 +0530

    Add files via upload

---
+++ chatApp/src/main/java/com/example/chatApp/ChatSocketHandler.java
@@ -0,0 +1,32 @@
+package com.example.chatApp;
+
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class ChatSocketHandler extends TextWebSocketHandler {
+
+    private final CopyOnWriteArrayList<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) {
+        sessions.add(session);
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        // Broadcast received message to all clients
+        for (WebSocketSession s : sessions) {
+            if (s.isOpen()) {
+                s.sendMessage(message);
+            }
+        }
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, org.springframework.web.socket.CloseStatus status) {
+        sessions.remove(session);
+    }
+}
